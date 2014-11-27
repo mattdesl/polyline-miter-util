9c02703e22388a71dd143cf11871557ca5939330
mattdesl <dave.des@gmail.com>
2014-11-27 14:33:26 -0500

    updating code

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-var vec = {
-    add: require('gl-vec2/add'),
-    set: require('gl-vec2/set'),
-    normalize: require('gl-vec2/normalize'),
-    subtract: require('gl-vec2/subtract')
-}
+var add = require('gl-vec2/add')
+var set = require('gl-vec2/set')
+var normalize = require('gl-vec2/normalize')
+var subtract = require('gl-vec2/subtract')
+var dot = require('gl-vec2/dot')
 
 var tmp = [0, 0]
 
 module.exports.computeMiter = function computeMiter(tangent, miter, lineA, lineB, halfThick) {
     //get tangent line
-    vec.add(tangent, lineA, lineB)
-    vec.normalize(tangent, tangent)
+    add(tangent, lineA, lineB)
+    normalize(tangent, tangent)
 
     //get miter as a unit vector
-    vec.set(miter, -tangent[1], tangent[0])
-    vec.set(tmp, -lineA[1], lineA[0])
+    set(miter, -tangent[1], tangent[0])
+    set(tmp, -lineA[1], lineA[0])
 
     //get the necessary length of our miter
-    return halfThick / vec.dot(miter, tmp)
+    return halfThick / dot(miter, tmp)
 }
 
 module.exports.normal = function normal(out, dir) {
     //get perpendicular
-    vec.set(out, -dir[1], dir[0])
+    set(out, -dir[1], dir[0])
     return out
 }
 
 module.exports.direction = function direction(out, a, b) {
     //get unit dir of two lines
-    vec.subtract(out, a, b)
-    vec.normalize(out, out)
+    subtract(out, a, b)
+    normalize(out, out)
     return out
 }
